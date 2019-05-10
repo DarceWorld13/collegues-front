ef8fef181167732f972d018deb9fd29f5ea355ee
Amadou <amadoudem@sfr.fr>
2019-05-10 17:29:08 +0200

    fin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,14 @@ export const environment = {
   production: true, 
   urlPosts: 'https://amadou--collegues-api.herokuapp.com/collegues/'
 };
+
+
+export const environmentAuth = {
+  production: false, 
+  urlPosts: 'https://amadou--collegues-api.herokuapp.com/auth'
+};
+
+export const environmentLogOut = {
+  production: false, 
+  urlPosts: 'https://amadou--collegues-api.herokuapp.com/logout'
+};
